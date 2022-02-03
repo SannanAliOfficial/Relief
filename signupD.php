44dea4d891be0565bd579cd8df919ea65d8f7aa4
SannanAliOfficial <88394994+SannanAliOfficial@users.noreply.github.com>
2022-02-03 08:49:39 +0500

    formas finalize

--- signupD.php
+++ signupD.php
@@ -25,7 +25,7 @@
       $DSpeciality = $_POST['DSpeciality'];
 
 
-      if(!empty($DFirst_name) && !empty($DEmail) && !empty($DPassword) && !empty($DRe_Password) && !empty(DPLast_name) && !empty($DPhone_number))
+      if(!empty($DFirst_name) && !empty($DEmail) && !empty($DPassword) && !empty($DRe_Password) && !empty($DLast_name) && !empty($DPhone_number))
       {
           
           $query = "INSERT INTO Doctors (DFirst_name, DLast_name, DUsername , DEmail, DPhone_number, DPassword, DRe_Password, DSpeciality) VALUES ('$DFirst_name','$DLast_name', '$DUsername','$DEmail', '$DPhone_number','$DPassword', '$DRe_Password' , '$DSpeciality')";
@@ -57,68 +57,123 @@
     <!---<title> Responsive Registration Form  </title>--->
     <link rel="stylesheet" href="styles.css">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   </head>
-<body>
-  <div class="container">
-    <div class="title">REGISTERATION</div>
-    <div class="content">
-      <form action="signupD.php" method="POST">
-        <div class="user-details">
-          <div class="input-box">
-            <span class="details">Fisrt Name</span>
-            <input type="text" placeholder="Enter your First name" name="DFirst_name"  required>
+  </head>
+  <body>
+    <form method="post" action="test.php" style=" position: fixed;">
+        <div class="container">
+          <p class="title">SIGNUP FORM</p><br>
+          <div>
+            <p class="input-box">
+              <label class="details" >First Name:    </label>
+              <input style="margin-bottom: 10px;
+              width: calc(100% / 2 - 20px); height: 30px;
+              outline: none;
+              font-size: 14px;
+              border-radius: 5px;
+              padding-left: 10px;
+              border: 1px solid #ccc;
+              border-bottom-width: 2px;" type="text" name="DFirst_name" >
+            </p>
+
+            <p class="input-box">
+              <label class="details">Last Name:     </label>
+              <input style="margin-bottom: 10px;
+              width: calc(100% / 2 - 20px); height: 30px;
+              outline: none;
+              font-size: 14px;
+              border-radius: 5px;
+              padding-left: 10px;
+              border: 1px solid #ccc;
+              border-bottom-width: 2px;" type="text" name="DLast_name" >
+            </p>
           </div>
-          <div class="input-box">
-            <span class="details">Last Name</span>
-            <input type="text" placeholder="Enter your Last name" name="DLast_name" required>
-          </div>
-          <div class="input-box">
-            <span class="details">Username</span>
-            <input type="text" placeholder="Enter your username" name="DUsername" required>
-          </div>
-          <div class="input-box">
-            <span class="details">Email</span>
-            <input type="text" placeholder="Enter your email" name="DEmail" required>
-          </div>
-          <div class="input-box">
-            <span class="details">Phone Number</span>
-            <input type="text" placeholder="Enter your number" name="DPhone_number" required>
-          </div>
-          <div class="input-box">
-            <span class="details">Password</span>
-            <input type="text" placeholder="Enter your password" name="DPassword" required>
-          </div>
-          <div class="input-box">
-            <span class="details">Confirm Password</span>
-            <input type="text" placeholder="Confirm your password" name="DRe_Password" required>
-          </div>
-        </div>
-        
-        <div class="input-box">
-          <span for="Dspeciality" name='DSpeciality'>Gender</span><br>
-          <select style="margin-bottom: 15px;
-          width: calc(100% / 2 - 20px); height: 45px;
-          outline: none;
-          font-size: 16px;
-          border-radius: 5px;
-          padding-left: 15px;
-          border: 1px solid #ccc;
-          border-bottom-width: 2px;" name="DSpeciality" id="DSpeciality" class="details">
-            <option value="C">CardioVascular</option>
+
+          <p class="input-box">
+            <label class="details">Username Name: </label>
+            <input style="margin-bottom: 10px;
+            width: calc(100% / 2 - 20px); height: 30px;
+            outline: none;
+            font-size: 14px;
+            border-radius: 5px;
+            padding-left: 10px;
+            border: 1px solid #ccc;
+            border-bottom-width: 2px;" type="text" name="DUsername" >
+          </p>
+
+          <p class="input-box">
+            <label class="details">Email:          </label>
+            <input style="margin-bottom: 10px;
+            width: calc(100% / 2 - 20px); height: 30px;
+            outline: none;
+            font-size: 14px;
+            border-radius: 5px;
+            padding-left: 10px;
+            border: 1px solid #ccc;
+            border-bottom-width: 2px;" type="Email" name="DEmail" >
+          </p>
+
+          <p class="input-box">
+            <label class="details">Phone number:  </label>
+            <input style="margin-bottom: 10px;
+            width: calc(100% / 2 - 20px); height: 30px;
+            outline: none;
+            font-size: 14px;
+            border-radius: 5px;
+            padding-left: 10px;
+            border: 1px solid #ccc;
+            border-bottom-width: 2px;" type="text" name="DPhone_number" >
+          </p>
+
+          <p class="input-box">
+            <label class="details">Password:      </label>
+            <input style="margin-bottom: 10px;
+            width: calc(100% / 2 - 20px); height: 30px;
+            outline: none;
+            font-size: 14px;
+            border-radius: 5px;
+            padding-left: 10px;
+            border: 1px solid #ccc;
+            border-bottom-width: 2px;" type="password" name="DPassword" >
+          </p>
+
+          <p class="input-box">
+            <label class="details">Re Password:   </label>
+            <input style="margin-bottom: 10px;
+            width: calc(100% / 2 - 20px); height: 30px;
+            outline: none;
+            font-size: 14px;
+            border-radius: 5px;
+            padding-left: 10px;
+            border: 1px solid #ccc;
+            border-bottom-width: 2px;" type="password" name="DRe_Password" >
+          </p>
+
+          <p class="input-box">
+            <span for="DSpeciality" name='DSpeciality' class="details">Gender</span>
+            <select style="margin-bottom: 10px;
+            width: calc(100% / 2 - 20px); height: 30px;
+            outline: none;
+            font-size: 14px;
+            border-radius: 5px;
+            padding-left: 10px;
+            border: 1px solid #ccc;
+            border-bottom-width: 2px;" name="DSpeciality" id="DSpeciality" >
+            <option value="C">Cardiologist</option>
             <option value="E">Endocrinologist</option>
             <option value="P">Pulmonologist</option>
             <option value="GP">General Physician</option>
-          </select>
+            <option value="O">Other</option>
+            </select>
+          </p>
+
+          <div class="button">
+            <input type="submit" value="REGISTER" >
+            <p class="text">Already have an account?    <a href="LoginP.php">SIGN IN</a></p>
+          </div>
         </div>
 
-        <div class="button">
-          <input type="submit" value="REGISTERED"><br><br>
-          <p class="text">Already have an account?<a href="LoginD.php">SIGN IN</a></p>
-        </div>
-        
-      </form>
-    </div>
-  </div>
 
-</body>
+    </form>
+
+  </body>
 </html>
--- signupP.php
+++ signupP.php
@@ -64,98 +64,165 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <link rel="icon" href="images/logo.png">
+<!doctype html>
+<html>
+<head>
+	<link rel="icon" href="images/logo.png">
     <title>SignUp Form | RELIEF </title>
     <meta charset="UTF-8">
-    <!---<title> Responsive Registration Form  </title>--->
     <link rel="stylesheet" href="styles.css">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   </head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">	
+</head>
+
 <body>
-  <div class="container">
-    <div class="title">SIGNUP FORM</div>
-    <div class="content">
-      <form method="post" action="signupP.php">
-        <div class="input-box">
-          <span class="details" name='PFirst_name' >First Name</span>
-          <input type="text" placeholder="Enter your First name" >
-        </div>
-        <div class="input-box">
-          <span class="details" name='PLast_name'>Last Name</span>
-          <input type="text" placeholder="Enter your name" >
-        </div>
-        <div class="input-box">
-          <span class="details" name='PUsername'>Username</span>
-          <input type="text" placeholder="Enter your username" >
-        </div>
-        <div class="input-box">
-          <span class="details" name='PDOB'>Year of Birth</span>
-          <input type="text" placeholder="YYYY" >
-        </div>
-        <div class="input-box">
-          <span class="details" name='PEmail'>Email</span>
-          <input type="email" placeholder="Enter your email" >
-        </div>
-        <div class="input-box">
-          <span class="details" name='PPhone_number'>Phone Number</span>
-          <input type="text" placeholder="Enter your number" >
-        </div>
-        <div class="input-box">
-          <span class="details" name='PAddress'>Address</span>
-          <input type="text" placeholder="Enter Your Adress" >
-        </div>
-          
-        <div class="input-box">
-          <span class="details" name='PPassword'>Password</span>
-          <input type="password" placeholder="Enter your password" >
-        </div>
-        <div class="input-box">
-          <span class="details" name='PRe_Password'>Confirm Password</span>
-          <input type="password" placeholder="Confirm your password" >
-        </div>
-        
-        
-        <div class="input-box">
-          <span for="PGender" name='PGender'>Gender</span><br>
-          <select style="margin-bottom: 15px;
-          width: calc(100% / 2 - 20px); height: 45px;
-          outline: none;
-          font-size: 16px;
-          border-radius: 5px;
-          padding-left: 15px;
-          border: 1px solid #ccc;
-          border-bottom-width: 2px;" name="PGender" id="PGender" class="details">
-            <option value="M">Male</option>
-            <option value="F">Female</option>
-            <option value="O">Others</option>
-          </select>
-        </div>
-        
+    <form method="post" action="test.php" style=" position: fixed;">
+        <div class="container">
+            <p class="title">SIGNUP FORM</p><br>
+            <div>
+                <p class="input-box">
+                    <label class="details" >First Name:    </label>
+                    <input style="margin-bottom: 10px;
+                    width: calc(100% / 2 - 20px); height: 30px;
+                    outline: none;
+                    font-size: 14px;
+                    border-radius: 5px;
+                    padding-left: 10px;
+                    border: 1px solid #ccc;
+                    border-bottom-width: 2px;" type="text" name="PFirst_name" >
+                </p>
 
-        <div>
-          <span class="details" style="font-weight: 500;">Diseases that Mainly applies to you:</span><br>
-          <input type="checkbox" name='PDiseases' id='D' value="D">
-          <label for="D">Diabates</label><br>
-          <input type="checkbox" name='PDiseases' id='BP' value="BP">
-          <label for="BP">Blood Pressure</label><br>
-          <input type="checkbox" name='PDiseases' id='HR' value="HR">
-          <label for="HR">Heart Rate Issues</label><br>
-          <input type="checkbox" name='PDiseases' id='OS' value="OS">
-          <label for="OS">Oxygen Saturation Issues</label>
+                <p class="input-box">
+                    <label class="details">Last Name:     </label>
+                    <input style="margin-bottom: 10px;
+                    width: calc(100% / 2 - 20px); height: 30px;
+                    outline: none;
+                    font-size: 14px;
+                    border-radius: 5px;
+                    padding-left: 10px;
+                    border: 1px solid #ccc;
+                    border-bottom-width: 2px;" type="text" name="PLast_name" >
+                </p>
+            </div>
+
+            <p class="input-box">
+                <label class="details">Username Name: </label>
+                <input style="margin-bottom: 10px;
+                width: calc(100% / 2 - 20px); height: 30px;
+                outline: none;
+                font-size: 14px;
+                border-radius: 5px;
+                padding-left: 10px;
+                border: 1px solid #ccc;
+                border-bottom-width: 2px;" type="text" name="PUsername" >
+            </p>
+
+            <p class="input-box">
+                <label class="details">Year of Birth: </label>
+                <input style="margin-bottom: 10px;
+                width: calc(100% / 2 - 20px); height: 30px;
+                outline: none;
+                font-size: 14px;
+                border-radius: 5px;
+                padding-left: 10px;
+                border: 1px solid #ccc;
+                border-bottom-width: 2px;" type="text" name="PDOB" >
+            </p>
+
+            <p class="input-box">
+                <label class="details">Email:          </label>
+                <input style="margin-bottom: 10px;
+                width: calc(100% / 2 - 20px); height: 30px;
+                outline: none;
+                font-size: 14px;
+                border-radius: 5px;
+                padding-left: 10px;
+                border: 1px solid #ccc;
+                border-bottom-width: 2px;" type="Email" name="PEmail" >
+            </p>
+
+            <p class="input-box">
+                <label class="details">Phone number:  </label>
+                <input style="margin-bottom: 10px;
+                width: calc(100% / 2 - 20px); height: 30px;
+                outline: none;
+                font-size: 14px;
+                border-radius: 5px;
+                padding-left: 10px;
+                border: 1px solid #ccc;
+                border-bottom-width: 2px;" type="text" name="PPhone_number" >
+            </p>
+
+            <p class="input-box">
+                <label class="details">Address:       </label>
+                <input style="margin-bottom: 10px;
+                width: calc(100% / 2 - 20px); height: 30px;
+                outline: none;
+                font-size: 14px;
+                border-radius: 5px;
+                padding-left: 10px;
+                border: 1px solid #ccc;
+                border-bottom-width: 2px;" type="text" name="PAddress" >
+            </p>
+
+            <p class="input-box">
+                <label class="details">Password:      </label>
+                <input style="margin-bottom: 10px;
+                width: calc(100% / 2 - 20px); height: 30px;
+                outline: none;
+                font-size: 14px;
+                border-radius: 5px;
+                padding-left: 10px;
+                border: 1px solid #ccc;
+                border-bottom-width: 2px;" type="password" name="PPassword" >
+            </p>
+
+            <p class="input-box">
+                <label class="details">Re Password:   </label>
+                <input style="margin-bottom: 10px;
+                width: calc(100% / 2 - 20px); height: 30px;
+                outline: none;
+                font-size: 14px;
+                border-radius: 5px;
+                padding-left: 10px;
+                border: 1px solid #ccc;
+                border-bottom-width: 2px;" type="password" name="PRe_Password" >
+            </p>
+
+            <p class="input-box">
+                <span for="PGender" name='PGender' class="details">Gender</span>
+                <select style="margin-bottom: 10px;
+                width: calc(100% / 2 - 20px); height: 30px;
+                outline: none;
+                font-size: 14px;
+                border-radius: 5px;
+                padding-left: 10px;
+                border: 1px solid #ccc;
+                border-bottom-width: 2px;" name="PGender" id="PGender" >
+                <option value="M">Male</option>
+                <option value="F">Female</option>
+                <option value="O">Others</option>
+                </select>
+            </p>
+            
+    
+            <p class="input-box">
+                <span class="details" >Diseases that Mainly applies to you:</span>
+                <input type="checkbox" name='PDiseases' id='D' value="D">
+                <label for="D">Diabates</label><br>
+                <input type="checkbox" name='PDiseases' id='BP' value="BP">
+                <label for="BP">Blood Pressure</label><br>
+                <input type="checkbox" name='PDiseases' id='HR' value="HR">
+                <label for="HR">Heart Rate Issues</label><br>
+                <input type="checkbox" name='PDiseases' id='OS' value="OS">
+                <label for="OS">Oxygen Saturation Issues</label>
+            </p>
+
+            <div class="button">
+                <input type="submit" value="REGISTER" >
+                <p class="text">Already have an account?    <a href="LoginP.php">SIGN IN</a></p>
+            </div>
         </div>
 
-        <div class="button">
-          <input type="submit" value="REGISTER"><br><br>
-          <p class="text">Already have an account?    <a href="LoginP.php">SIGN IN</a></p>
-        </div>
-          
-        
-      </form>
-    </div>
-  </div>
 
+    </form>
 </body>
-</html>
